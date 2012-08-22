ea57d493ee21622d08bab308749d2e5b5d578592
Daniel <daniel.zukowski@gmail.com>
2012-08-22 17:38:49 -0600

    mysql2 gem for production

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ ssh_options[:forward_agent] = true
 set :application, "spree-edge"
 set :repository,  "git@github.com:AktionLab/spree-edge.git"
 set :scm, :git
-set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/home/daniel/#{application}"
 set :rvm_ruby_string, '1.9.3-p0@spree'
 set :rvm_type, :user
 set :rails_env, 'production'
@@ -15,7 +15,7 @@ set :use_sudo, false
 set :keep_releases, 3
 set :ec2_server, 'bzlabs.org'
 set :port, 2222
-set :symlinks, %w(config/database.yml config/unicorn.rb)
+set :configs, %w(config/database.yml config/unicorn.rb)
 
 role :web, "#{ec2_server}"                   # Your HTTP server, Apache/etc
 role :app, "#{ec2_server}"                   # This may be the same as your `Web` server
@@ -23,10 +23,10 @@ role :db,  "#{ec2_server}", :primary => true # This is where Rails migrations wi
 
 # if you want to clean up old releases on each deploy uncomment this:
 #before 'deploy:assets:precompile', 'deploy:symlink_shared'
-after 'deploy:symlink_shared', 'deploy:rake_tasks'
+after 'deploy:config_files', 'deploy:rake_tasks'
 after 'deploy:rake_tasks', 'nginx:config'
 after 'nginx:config', 'nginx:reload'
-after 'deploy', 'deploy:symlink_shared'
+after 'deploy', 'deploy:config_files'
 #after 'deploy', 'deploy:cleanup'
 
 namespace :deploy do
@@ -34,8 +34,8 @@ namespace :deploy do
     #task(action) { run "cd #{current_path} && script/unicorn #{action}" }
   end
 
-  task :symlink_shared, :except => { :no_release => true } do
-    run(symlinks.map {|link| "ln -nfs #{shared_path}/#{link} #{release_path}/#{link}"}.join(' && '))
+  task :config_files, :except => { :no_release => true } do
+    run(configs.map {|file| "cp #{shared_path}/#{file} #{release_path}/#{file}"}.join(' && '))
   end
 
   task :rake_tasks do
