7848536e430695994d61d23c1e571df718ccc785
Daniel <daniel.zukowski@gmail.com>
2012-08-22 17:03:25 -0600

    change gemset string

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :application, "spree-edge"
 set :repository,  "git@github.com:AktionLab/spree-edge.git"
 set :scm, :git
 set :deploy_to, "/var/www/#{application}"
-set :rvm_ruby_string, '1.9.3-p0@spree-edge'
+set :rvm_ruby_string, '1.9.3-p0@spree'
 set :rvm_type, :user
 set :rails_env, 'production'
 set :use_sudo, false
@@ -19,15 +19,16 @@ role :app, "#{ec2_server}"                   # This may be the same as your `Web
 role :db,  "#{ec2_server}", :primary => true # This is where Rails migrations will run
 
 # if you want to clean up old releases on each deploy uncomment this:
-before 'deploy:assets:precompile', 'deploy:symlink_shared'
+#before 'deploy:assets:precompile', 'deploy:symlink_shared'
 after 'deploy:symlink_shared', 'deploy:rake_tasks'
 after 'deploy:rake_tasks', 'nginx:config'
 after 'nginx:config', 'nginx:reload'
+after 'deploy', 'deploy:symlink_shared'
 after 'deploy', 'deploy:cleanup'
 
 namespace :deploy do
   %w(start stop restart).each do |action|
-    task(action) { run "cd #{current_path} && script/unicorn #{action}" }
+    #task(action) { run "cd #{current_path} && script/unicorn #{action}" }
   end
 
   task :symlink_shared, :except => { :no_release => true } do
